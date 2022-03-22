c54b7656d6c3eba3e084f1de73da367b6ba372c8
csaby <csabyn@gmail.com>
2022-03-22 09:27:02 +0200

    Tema 2

---
+++ tema_2.py
@@ -0,0 +1,6 @@
+# 1.Să se scrie o funcție care primește un număr nedefinit de parametrii și să se calculeze suma parametrilor care reprezintă numere întregi sau reale.
+
+# 2.ă se scrie o funcție recursivă care primește ca parametru un număr întreg și returnează:
+
+# 3.Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta este un număr întreg, altfel returnează valoarea 0.
+
